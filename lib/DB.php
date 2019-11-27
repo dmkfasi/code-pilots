a60f1ab04e87770459f1fd876bf6f3bc4b9a5e04
Andrei Ivanov <dmkfasi@gmail.com>
2019-11-27 23:54:19 +0300

    Fixed DSN typo

--- lib/DB.php
+++ lib/DB.php
@@ -9,10 +9,10 @@ class DB extends PDO {
 		$cfg = Config::getInstance();
 
 		// Construct DSN since ini file can't contain equal sign in a value
-		$dsn = "{$cfg->driver}:{$cfg->dbname};host={$cfg->host}";
+		$dsn = "{$cfg->driver}:dbname={$cfg->dbname};host={$cfg->host}";
 
 		try {
-	    parent::__construct($sn, $cfg->username, $cfg->password);
+	    parent::__construct($dsn, $cfg->username, $cfg->password);
 		} catch (PDOException $e) {
 			throw new ApplicationException('Unable to instantiate PDO driver: ' . $e->getMessage());
 		}
@@ -25,4 +25,21 @@ class DB extends PDO {
 
 		return self::$instance;
 	}
+
+	public function execute(string $sql, array $params = []) {
+    try {
+      $sth = $this->prepare($sql);                                                                                                                                           
+      $sth->execute($params);
+    } catch (PDOException $e) {
+      throw new ApplicationException('Unable to prepare SQL: ' . $e->getMessage());
+    }
+
+    return $sth;
+	}
+
+  public function getResults(string $sql, array $params = []) {
+    $sth = $this->execute($sql, $params);
+
+		return $sth->fetchAll(PDO::FETCH_ASSOC);
+  }
 }
