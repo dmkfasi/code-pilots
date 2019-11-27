bc4452531477b5a21f89e1893c2faffa29c72955
Andrei Ivanov <dmkfasi@gmail.com>
2019-11-27 23:55:51 +0300

    Basic handling

--- lib/Request.php
+++ lib/Request.php
@@ -2,4 +2,27 @@
 
 class Request {
 
+	public function __construct(Context $ctx) {
+		$this->context = $ctx;
+
+		$method = $this->context->getRequestMethod();
+
+		switch ($method) {
+			case 'HEAD':
+			case 'PUT':
+			default:
+				throw new ApplicationException("HTTP method '{$m}' is not supported");
+				break;
+
+			case 'GET':
+			case 'POST':
+				$this->runPost();
+				break;
+		}
+	}
+
+	private function runPost() {
+		debug($this->context->getAction());
+	}
+
 }
