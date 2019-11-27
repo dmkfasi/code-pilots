5398f31295722571f2c8761ff30f035b46d39d02
Andrei Ivanov <dmkfasi@gmail.com>
2019-11-27 21:09:11 +0300

    WIP

--- index.php
+++ index.php
@@ -4,15 +4,11 @@
 require_once 'lib/bootstrap.php';
 
 // Basic HTTP router
-$res = new Response();
+$req = new Request();
 
+// Basic HTTP response
+$res = new Response();
 $res->setStatus('ok');
 $res->setMessage('Everything is fine');
-
-//debug($res->toJson());
-
 $res->dispatch();
 
-function debug($o) {
-	print_r("<pre>{$o}</pre>");
-}
