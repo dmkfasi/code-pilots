b0a409944035cee778d9132f08b328565bc66813
Andrei Ivanov <dmkfasi@gmail.com>
2019-11-28 15:34:14 +0300

    Trying to unify call arguments

--- app/AppTable.php
+++ app/AppTable.php
@@ -1,12 +1,20 @@
 <?php
 
+// TODO introduce an Interface to all App objects
 class AppTable {
 
 	public $table = '';
 	public $id = null;
 
-	// Blacklist tables that are not allowed for direct access
+	// Blacklist database objects that are not allowed for direct access
 	private $blacklist = [ 'User' ];
+
+	// Application Context
+	private $context = null;
+
+	public function __construct(Context $ctx) {
+		$this->context = $ctx;
+	}
 
 	public function getContent() {
 
@@ -14,18 +22,18 @@ class AppTable {
 
 		// Not possible to bind a table or column name with PDO
 		if ($this->isAllowed()) {
-			$base_sql = "SELECT * FROM {$this->table}";
+			$base_sql = "SELECT * FROM {$this->context->table}";
 		}
 
 		// TODO empty set handling
 		if ($this->id !== null) {
-			return $db->getResults("{$base_sql} WHERE ID = :id", [ 'id' => $this->id ]);
+			return $db->getResults("{$base_sql} WHERE ID = :id", [ 'id' => $this->context->id ]);
 		} else {
 			return $db->getResults($base_sql);
 		}
 	}
 
 	public function isAllowed() {
-		return !in_array($this->table, $this->blacklist);
+		return !in_array($this->context->table, $this->blacklist);
 	}
 }
