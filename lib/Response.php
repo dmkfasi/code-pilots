f64ff7d6bc7e3a10031f7476300be4e7d4493a5a
Andrei Ivanov <dmkfasi@gmail.com>
2019-11-28 15:23:12 +0300

    Output data overload from Context added

--- lib/Response.php
+++ lib/Response.php
@@ -6,18 +6,23 @@
 
 class Response {
 
-  public $status = '';
+  public $status = 'ok';
   public $payload = [];
-  public $message = '';
+  public $message = 'Everything is fine';
   private $content_type = 'application/json';
 
   public function setStatus(string $status) {
     $this->status = $status;
   }
 
-  public function setPayload(array $payload) {
-    $this->payload = $payload;
-  }
+  public function setPayload($payload) {
+		// Convert any output to an array
+		if (!is_array($payload)) {
+			$payload = [ $payload ];
+		}
+
+		$this->payload = $payload;
+	}
 
   public function setMessage(string $message) {
     $this->message = $message;
@@ -38,6 +43,33 @@ class Response {
     header('Content-Length: ' . strlen($content));
     die($content);
   }
+
+	public function setup(Context $ctx) {
+		// Collect output buffer from Context to
+		// convert it to an actual HTTP response
+		$output = $ctx->getContent();
+
+		// Override response type and data if set with Context
+		if (!empty($output['content_type'])) {
+			$this->setContentType($output['content_type']);
+		}
+
+		if (!empty($output['message'])) {
+			$this->setMessage($output['message']);
+		}
+
+		if (!empty($output['status'])) {
+			$this->setStatus($output['status']);
+		}
+
+		// Whenever there is no overriden payload,
+		// use output object directly
+		if (!empty($output['payload'])) {
+			$this->setPayload($output['payload']);
+		} else {
+			$this->setPayload($output);
+		}
+	}
 
   public function __toString() {
     return $this->message;
